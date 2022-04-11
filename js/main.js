55059b718b6cc04ffd3007871033ed1bc374ee71
Malik Felix <malikfelix50@gmail.com>
2022-04-10 23:05:01 -0400

    add temperature, condition, icon

--- js/main.js
+++ js/main.js
@@ -10,6 +10,12 @@ fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${weather}`)
   .then(data => {
     console.log(data);
     document.querySelector(".location").innerText = `${data.location.name}, ${data.location.region}, ${data.location.country}`;
+    
+    document.querySelector('.temperature').innerText = `${Math.floor(data.current.temp_f)}\xB0`;
+
+    document.querySelector('.condition').innerText = data.current.condition.text;
+    
+    document.querySelector('img').src= data.current.condition.icon;
   })
   .catch(err => {
     console.log(`error ${err}`)
