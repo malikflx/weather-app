edbd494bf7efc898656dac664bfe68dc424cfb06
Malik Felix <malikfelix50@gmail.com>
2022-04-10 22:25:51 -0400

    return weather location (city/town, region, country)

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 import API_KEY from "../config.js";
 console.log(`Your API Key is ${API_KEY}`);
+document.querySelector('button').addEventListener('click', getWeather);
+
+function getWeather() {
+  let weather = document.querySelector('input').value;
+
+fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${weather}`)
+  .then(response => response.json()) // parse response as JSON
+  .then(data => {
+    console.log(data);
+    document.querySelector(".location").innerText = `${data.location.name}, ${data.location.region}, ${data.location.country}`;
+  })
+  .catch(err => {
+    console.log(`error ${err}`)
+  });
+}
